de82658e2f287a7ae79ca22044fa20ff4a7faae6
ksaito <kitsit.39@gmail.com>
2025-06-03 08:36:29 +0900

    Add color Face settings

--- lib/irb/theme/huston.rb
+++ lib/irb/theme/huston.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
-require_relative "huston/version"
+Reline::Face.config(:default) do |conf|
+  conf.define(:default, foreground: "#eef0f9", background: "#17191e")
+  conf.define(:enhanced, foreground: "#17191e", background: "#acafff")
+  conf.define(:scrollbar, foreground: "#4bf3c8", background: "#17191e")
+end
 
-module Irb
-  module Theme
-    module Huston
-      class Error < StandardError; end
-      # Your code goes here...
-    end
-  end
+Reline::Face.config(:completion_dialog) do |conf|
+  conf.define(:default, foreground: "#eef0f9", background: "#17191e")
+  conf.define(:enhanced, foreground: "#17191e", background: "#acafff")
+  conf.define(:scrollbar, foreground: "#4bf3c8", background: "#17191e")
 end
